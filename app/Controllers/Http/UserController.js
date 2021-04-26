04fc9d32e5dc9a5464e57b078a501e3ee056dfc4
Gabriel de La Porta <gabrieldelaporta@beotto.com>
2021-04-26 01:18:30 -0300

    User authentication added and fix forms layout

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -12,14 +12,12 @@ class UserController {
   }
 
   async store ({ request, response, session }) {
-    // validate form input
     const validation = await validate(request.all(), {
       username: 'required|unique:users',
       email: 'required|email|unique:users,email',
       password: 'required'
     })
 
-    // show error messages upon validation fail
     if (validation.fails()) {
       console.log(validation.messages())
       session.withErrors(validation.messages())
@@ -39,7 +37,7 @@ class UserController {
     return response.redirect('back')
   }
 
-  async login ({ request, response, session }) {
+  async login ({ request, response, auth }) {
     const validation = await validate(request.all(), {
       email: 'required|email',
       password: 'required'
@@ -47,26 +45,23 @@ class UserController {
 
     if (validation.fails()) {
       console.log(validation.messages())
-      session.withErrors(validation.messages())
+      auth.withErrors(validation.messages())
         .flashAll()
 
       return response.redirect('back')
     }
-    const user = await User.query().where({email: request.input('email'), password: request.input('password')})
 
-    if (user){
-      session.put('email', request.input('email'))
-      return response.redirect('/home')
-    }
+    const { email, password } = request.all()
 
-    return response.redirect('back')
+    await auth.attempt(email, password)
+
+    return response.redirect('/home')
   }
 
   async destroy ({ params, session, response }) {
     const user = await User.find({email: params.email})
     await user.delete()
 
-    // Fash success message to session
     session.flash({ notification: 'User deleted!' })
 
     return response.redirect('back')
